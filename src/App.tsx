9edc331434122928d1307d3c3af55fd25a1fe860
Masood Ahmad <112746559+MSD118@users.noreply.github.com>
2025-02-21 11:35:55 +0530

    refactor App component to use DemoWindow for demo navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { RefAsPropDemo } from './RefAsProp/RefAsPropDemo'
-import { RefCleanUpDemo } from './RefDemo/RefCleanUpDemo'
-import { LikeButton } from './UseOptimistic/LikeButton'
-import { UpdateName } from './UseOptimistic/UpdateName'
-import { FormDemo } from './FormDemo/FormDemo'
-import { UseActionStateDemo } from './UseActionStateDemo'
-
-const DEMO = [
-  RefCleanUpDemo,
-  UpdateName,
-  LikeButton,
-  RefAsPropDemo,
-  FormDemo,
-  UseActionStateDemo,
-]
+import { DemoWindow } from './DemoWindow'
 
 function App() {
-  return (
-    <div className='p-4 w-full min-h-screen'>
-      <div className='flex flex-col items-center '>
-        {DEMO.map((Component, index) => (
-          <div key={index} className='mb-4'>
-            <Component />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+  return <DemoWindow />
 }
 
 export default App
---
+++ src/DemoWindow.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { FormDemo } from './FormDemo/FormDemo'
+import { RefAsPropDemo } from './RefAsProp/RefAsPropDemo'
+import { RefCleanUpDemo } from './RefDemo/RefCleanUpDemo'
+import { UseActionStateDemo } from './UseActionStateDemo'
+import { LikeButton } from './UseOptimistic/LikeButton'
+import { UpdateName } from './UseOptimistic/UpdateName'
+
+const DEMO = [
+  { title: 'Ref Clean Up', component: RefCleanUpDemo },
+  { title: 'Update Name', component: UpdateName },
+  { title: 'Like Button', component: LikeButton },
+  { title: 'Ref As Prop', component: RefAsPropDemo },
+  { title: 'Form Demo', component: FormDemo },
+  { title: 'Use Action State', component: UseActionStateDemo },
+]
+
+export const DemoWindow = () => {
+  const [page, setPage] = useState(0)
+  const handleNext = () => {
+    if (DEMO.length === page + 1) return
+    setPage((prev) => prev + 1)
+  }
+  const handlePrev = () => {
+    if (page === 0) return
+    setPage((prev) => prev - 1)
+  }
+  return (
+    <div className='p-4 w-full min-h-screen'>
+      <div className='flex gap-2 mb-4'>
+        <button
+          onClick={handlePrev}
+          className='px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700'
+        >
+          Prev
+        </button>
+        <button
+          onClick={handleNext}
+          className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'
+        >
+          Next
+        </button>
+      </div>
+      <div className='flex flex-col items-center '>
+        {DEMO.map(({ title, component: Component }, index) =>
+          page === index ? (
+            <div key={index}>
+              <h2 className='text-4xl font-bold text-center mb-10'>{title}</h2>
+              <Component />
+            </div>
+          ) : null,
+        )}
+      </div>
+    </div>
+  )
+}
